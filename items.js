91ff7efd24554da5735405e32d6d6efad09e596c
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-26 13:27:45 -0400

    updated my items display view

--- items.js
+++ items.js
@@ -26,37 +26,96 @@ function show(data) {
   // this uses a combination of "HTML building" DOM methods (the document createElements) and
   // simple string interpolation (see the 'a' tag on title)
   // both are valid ways of building the html.
-  const ul = document.getElementById('items');
-  const list = document.createDocumentFragment();
-  //console.log('Data:', data);
+  const container = document.getElementById('container');
+  // const list = document.createDocumentFragment();
+  // let title = document.getElementById('title')
+  // let img = document.getElementById('img');
+  // let model = document.getElementById('model');
+  // let barCode = document.getElementById('barCode');
+  // let location = document.getElementById('location');
+  
+  console.log('Data:', data);
 
   data.map(function (item) {
     console.log('Item:', item);
-    let li = document.createElement('li');
-    let title = document.createElement('h3');
-    let body = document.createElement('p');
-    let image = document.createElement('img');
-    let barCode = document.createElement('p');
-    let location = document.createElement('p');
-    title.innerHTML = `<a href="./itemDetail.html?id=${item.id}">${item.type}</a>`;
-    body.innerHTML = `Model Number: ${item.model}`;
-    barCode.innerHTML = `Baer Code: ${item.barCodeNumber}`;
-    location.innerHTML = `Current Location: ${item.locationId}`;
-    image.src = `${item.imageUrl}`;
-    image.width = 300;
-    image.height = 300;
+    // let li = document.createElement('li');
+    // let title = document.createElement('h3');
+    //create a card div
+    const card = document.createElement('div')
+    card.className = 'card m-3 bg-light'
+    card.style.width = '18rem'; //can change this it optional
+
+    // Create image
+    const img = document.createElement('img');
+    img.className = 'card-img-top';
+    img.src = item.imageUrl;
+    img.alt = item.type;
+    
+   // Create card body
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  
+  // Title with link
+  const title = document.createElement('h5');
+  title.className = 'card-title';
+  title.innerHTML = `<a href="./itemDetail.html?id=${item.id}" class="text-decoration-none">${item.type}</a>`;
+
+  // Model text
+  const model = document.createElement('p');
+  model.className = 'card-text';
+  model.innerText = `Model: ${item.model}`;
+  
+  // Barcode text
+  const barCode = document.createElement('p');
+  barCode.className = 'card-text';
+  barCode.innerText = `Barcode: ${item.barCodeNumber}`;
+  
+  // Location text
+  const location = document.createElement('p');
+  location.className = 'card-text';
+  location.innerText = `Location ID: ${item.locationId}`;
+
+  // Put everything together
+  cardBody.appendChild(title);
+  cardBody.appendChild(model);
+  cardBody.appendChild(barCode);
+  cardBody.appendChild(location);
+  card.appendChild(img);
+  card.appendChild(cardBody);
+  
+  // Add card to container
+  container.appendChild(card);
+
+    // title.innerHTML = `<a href="./itemDetail.html?id=${item.id}">${item.type}</a>`;
+    // model.innerHTML = item.model;
+    // barCode.innerHTML = item.barCode;
+    // location.innerHTML = item.locationId;
+    // img.createElement('img')
+    // img.src = item.imageUrl;
+    // let body = document.createElement('p');
+    // let image = document.createElement('img');
+    // let barCode = document.createElement('p');
+    // let location = document.createElement('p');
+   // title.innerHTML = `<a href="./itemDetail.html?id=${item.id}">${item.type}</a>`;
+    // body.innerHTML = `Model Number: ${item.model}`;
+    // barCode.innerHTML = `Baer Code: ${item.barCodeNumber}`;
+    // location.innerHTML = `Current Location: ${item.locationId}`;
+    // image.src = `${item.imageUrl}`;
+    // image.width = 300;
+    // image.height = 300;
 
 
-    li.appendChild(title);
-    li.appendChild(image)
-    li.appendChild(body);
-    li.appendChild(barCode);
-    li.appendChild(location);
-    list.appendChild(li);
+    
+    // li.appendChild(image)
+    // li.appendChild(title);
+    // li.appendChild(body);
+    // li.appendChild(barCode);
+    // li.appendChild(location);
+    // list.appendChild(li);
   });
 
-  ul.appendChild(list);
-  ul.style.listStyleType = 'none';
+  // ul.appendChild(list);
+  // ul.style.listStyleType = 'none';
 }
 
 fetchData();
